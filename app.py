67d6328757ba8656ca2a6d4baa9253c34d1a492f
sln171nls <103336510+sln171nls@users.noreply.github.com>
2022-11-30 20:18:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 st.write("""
 # My Graded Assignment
 """)
+st.header('User Input Parameters')
 def user_input_features():
-    number1 = st.number_input("AMT_NUMERATOR")
-    number2 = st.number_input("AMT_DENOMINATOR")
+    number1 = st.number_input("AMT_NUMERATOR",min_value=2000000.0,max_value=2000000.0)
+    number2 = st.number_input("AMT_DENOMINATOR",min_value=2000000.0,max_value=2000000.0)
