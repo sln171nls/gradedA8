f46f4d4135e46b1adb3526c7167bb2e9dbe7bf3a
sln171nls <103336510+sln171nls@users.noreply.github.com>
2022-11-30 20:10:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.write("""
 # My Graded Assignment
 """)
+def user_input_features():
+    
+    number1 = st.number_input("AMT_NUMERATOR")
+    number2 = st.number_input("AMT_DENOMINATOR")
